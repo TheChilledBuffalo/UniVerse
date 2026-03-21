6c3ba363b2cf620107635962cf5feee63f497559
TheChilledBuffalo <abhishekgs.tvm@gmail.com>
2026-03-22 00:41:08 +0530

    chore: add eslint better tailwind & other required packages

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -2,12 +2,21 @@ import plugInQuery from "@tanstack/eslint-plugin-query";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   plugInQuery.configs["flat/recommended"],
+  {
+    extends: [eslintPluginBetterTailwindcss.configs.recommended],
+    settings: {
+      "better-tailwindcss": {
+        entryPoint: "app/globals.css",
+      },
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -18,4 +27,4 @@ const eslintConfig = defineConfig([
   ]),
 ]);
 
-export default [eslintConfig, eslintConfigPrettier];
+export default defineConfig([...eslintConfig, eslintConfigPrettier]);
